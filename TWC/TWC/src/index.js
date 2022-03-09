8dcf79f81ee23478fd7e4e93130d65df546e725c
Rahman Niftaliyev <rahman.n1234567@gmail.com>
2022-03-09 15:18:14 +0400

    inject js for index js clean code

--- TWC/TWC/src/index.js
+++ TWC/TWC/src/index.js
@@ -1,30 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './features/root/App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import inject from './inject'
 import 'alertifyjs/build/css/alertify.min.css';
 import 'animate.css/animate.min.css';
 import 'animate.css';
 import './index.css';
 
-const rootElement = document.getElementById('root');
 
-const renderer = () => {
-    return {
-        obj: ReactDOM.render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </Provider>,
-            rootElement
-        )
-    };
-};
 
-renderer();
+inject(ReactDOM,App,Provider,BrowserRouter,store);
 
 
 // If you want to start measuring performance in your app, pass a function
---
+++ TWC/TWC/src/inject.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const inject = (
+    ParamsDom, ParamsApp, ParamsProvider, ParamsRouter, ParamsStore
+) =>
+{
+    const rootElement = document.getElementById( 'root' );
+    return {
+        obj: ParamsDom.render( <ParamsProvider store={ParamsStore}>
+            <ParamsRouter>
+                <ParamsApp />
+            </ParamsRouter>
+        </ParamsProvider>, rootElement )
+    }
+}
+
+
+
+export default inject
