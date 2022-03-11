61c16255f3f4f384d59016b0aea4196a5560a146
Rahman Niftaliyev <rahman.n1234567@gmail.com>
2022-03-11 10:50:58 +0400

    DLl conf completed

--- TWC/TWC/aspnetcore-react.js
+++ TWC/TWC/aspnetcore-react.js
@@ -2,6 +2,7 @@
 // This script configures the .env.development.local file with additional environment variables to configure HTTPS using the ASP.NET Core
 // development certificate in the webpack development proxy.
 
+"use strict";
 const fs = require('fs');
 const path = require('path');
 
